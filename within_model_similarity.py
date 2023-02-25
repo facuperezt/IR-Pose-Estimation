ef53b27823ffdc7c3cab6183d00476b91f52f1bc
FaQ <facuperezt@googlemail.com>
2023-02-25 11:54:43 +0100

    added offset agnostic option

--- within_model_similarity.py
+++ within_model_similarity.py
@@ -18,10 +18,11 @@ def parse_args():
     parser.add_argument('-atol', '--absolute_tolerance', type=float, required= False, default= 0.1, help= 'Maximum absolute deviation from original slice.')
     parser.add_argument('-v', '--verbose', action='store_true', required=False, help= 'Flag to print Dissimilarity scores w.r.t. candidates.')
     parser.add_argument('-vis', '--visualize', action='store_true', help= 'Whether to visualize each slice (not recommended for large amounts of slices.)')
+    parser.add_argument('--allow_offset', action='store_true', help= 'Consider two slices with constant offset as the same slice.')
 
     return parser.parse_args()
 
-def find_similar_slices(model : str, slice: str, folder_path: str = "data/ss_lookup_table/dict", rtol: float  =  0.1, atol : float = 5, verbose= False):
+def find_similar_slices(model : str, slice: str, folder_path: str = "data/ss_lookup_table/dict", rtol: float  =  0.1, atol : float = 5, verbose= False, offset_allowed = False):
     """Finds similar slices within the same model.
 
     Args:
@@ -43,7 +44,8 @@ def find_similar_slices(model : str, slice: str, folder_path: str = "data/ss_loo
     ch = [c for c in chosen_fd.values() if c is not None]
     if verbose:
         print(ch[2:])
-    out = []
+    out_slices = []
+    slice_offsets = []
     for file in listdir(folder_path):
         if '_'.join(os.path.splitext(file)[0].split('_')[:-1]) == model:
             with open(os.path.join(folder_path, file), 'rb') as compare_file:
@@ -51,23 +53,31 @@ def find_similar_slices(model : str, slice: str, folder_path: str = "data/ss_loo
             co = [c for c in compare_fd.values() if c is not None]
             if len(ch) != len(co):
                 continue # No need to compare slices that don't have similar geometrical properties
-            closeness_per_class = np.array([np.allclose(a,b, rtol= rtol, atol= atol) for a,b in zip(co, ch)]) # Original slice has to be "b" parameter for np.allclose, see Notes in https://numpy.org/doc/stable/reference/generated/numpy.allclose.html
-            if closeness_per_class.all(): # Make sure that all geometrical properties are in similar positions
+            if offset_allowed:
+                co_array, ch_array = np.squeeze(np.array(co[2:])), np.squeeze(np.array(ch[2:]))
+                offset= co_array - ch_array # offset from main slice to other slice
+                co_array -= offset[0] # If offsets are consistent, then substracting by the first one should make both slices be the same
+                closeness_per_class = np.allclose(co_array, ch_array, rtol=rtol, atol=atol)
+                if closeness_per_class: slice_offsets.append([*offset[0]])
+            else:
+                closeness_per_class = np.array([np.allclose(a,b, rtol= rtol, atol= atol) for a,b in zip(co, ch)]).all() # Original slice has to be "b" parameter for np.allclose, see Notes in https://numpy.org/doc/stable/reference/generated/numpy.allclose.html
+                if closeness_per_class: slice_offsets.append([0,0,0])
+            if closeness_per_class: # Make sure that all geometrical properties are in similar positions
                 if file != '_'.join([model, slice + '.pkl']):
                     if verbose:
                         print('\nFor obj: ', file)
                         print('\tDissimilarity Score = ', '{:.2f}'.format(np.sum(sum([abs(ab) for ab in [a - b for a,b in zip(co,ch)]]))))
                         print(co[2:])
-                    out.append(os.path.splitext(file)[0])
+                    out_slices.append(os.path.splitext(file)[0])
 
-    return out
+    return out_slices, slice_offsets
 
-def visualize_slices(slices):
+def visualize_slices(slices, data_folder = 'data'):
     pcds = []
     cmap_names = ['Spectral', 'coolwarm', 'bwr', 'seismic']
     for _slice, cmap_name in zip(slices, cmap_names):
-        pcd = o3d.io.read_point_cloud(f'data/train/welding_zone/{_slice}.pcd')
-        labels = load_pcd_data(f'data/train/welding_zone/{_slice}.pcd')[:, 3].astype(int)
+        pcd = o3d.io.read_point_cloud(f'{data_folder}/train/welding_zone/{_slice}.pcd')
+        labels = load_pcd_data(f'{data_folder}/train/welding_zone/{_slice}.pcd')[:, 3].astype(int)
         nr_unique_labels = np.unique(labels).shape[0]
         cmap = plt.get_cmap(cmap_name, nr_unique_labels)
         colors_choice = np.array([np.array(cmap(i/nr_unique_labels))[:3] for i in range(cmap.N)])
@@ -88,14 +98,14 @@ if __name__ == '__main__':
     found = []
     for _slice in slices:
         if _slice in found: continue
-        if args.visualize: visualize_slices([args.model + '_' + _slice])
-        s_s = find_similar_slices(args.model, _slice, args.folder_path, args.relative_tolerance, args.absolute_tolerance, args.verbose)
+        if args.visualize: visualize_slices([args.model + '_' + _slice], data_folder = args.folder_path.split('/')[0])
+        similar_slices, offsets = find_similar_slices(args.model, _slice, args.folder_path, args.relative_tolerance, args.absolute_tolerance, args.verbose, args.allow_offset)
         found.append(_slice)
-        similar = [s.split('_')[-1] for s in s_s]
+        similar = [s.split('_')[-1] for s in similar_slices]
         found.extend(similar)
         for _similar_slice in similar:
             similar_onehot_matrix[int(_slice), int(_similar_slice)] = True
-        if args.visualize: visualize_slices(s_s)
+        if args.visualize: visualize_slices(similar_slices)
     similar_onehot_matrix = np.maximum(similar_onehot_matrix, similar_onehot_matrix.transpose())
     similar_onehot_matrix = np.maximum(similar_onehot_matrix, np.eye(similar_onehot_matrix.shape[0]))
     np.save(args.model+'_similarity_onehot_matrix.npy', similar_onehot_matrix)
