bd0e66effb86c9f0e9fac6cf2c49e19ff5386572
linhdoan8 <linh4138@yahoo.de>
2023-04-29 16:07:51 +0200

    merging

--- visualize_mesh.py
+++ visualize_mesh.py
@@ -5,7 +5,30 @@ import numpy as np
 import open3d as o3d
 import os
 
+def find_collisions(torch_mesh_path, slice_pcl):
+    """computes collision between slice PCL and torch mesh
+
+    Args:
+        torch_mesh_path (_type_): _description_
+        slice_pcl (_type_): _description_
+        query_points (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    scene = o3d.t.geometry.RaycastingScene()
+    mesh = o3d.io.read_triangle_mesh(torch_mesh_path)
+    mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
+    _ = scene.add_triangles(mesh)  # we do not need the geometry ID for mesh
+    occupancy = scene.compute_occupancy(slice_pcl)
+
+    return occupancy.any()
+
 if __name__ == "__main__":
+    pass
+
+
+def visualize_mesh():
     f = open('obj_0_pts.txt', 'r')
     objs = f.readlines()
     f.close()
